e31d9354b40c67df06af60dc2604ff0b36b2a476
aalkazemi <65915845+aalkazemi@users.noreply.github.com>
2020-06-12 11:06:24 +0300

    Add files via upload

---
+++ students.js
@@ -0,0 +1,30 @@
+export const students = [
+{
+	id: '4447',
+	name: 'Amina Alkazemi',
+	email: 'akazemi@auk.edu.kw',
+	major: 'Computer Science',
+	gpa: 3.4
+},
+{
+	id: '3339',
+	name: 'Anfal Rashid',
+	email: 'arashid@auk.edu.kw',
+	major: 'Computer Science',
+	gpa: 3.2
+},
+{
+	id: '4563',
+	name: 'Reem Al-Hilali',
+	email: 'rhilalli@auk.edu.kw',
+	major: 'Finance',
+	gpa: 3.5
+},
+{
+	id: '42327',
+	name: 'Nisreen Nour Al-Deen',
+	email: 'ndeen@auk.edu.kw',
+	major: 'Journalism',
+	gpa: 2.9
+}
+];
